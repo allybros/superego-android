8fb8175f15ec3c6287ac0911d47e19e06736c5ec
orcun <orcunkamiloglu@gmail.com>
2019-08-16 17:25:41 +0300

    Fix some pages

---
+++ app/src/main/java/com/allybros/superego/activity/ResultsFragment.java
@@ -0,0 +1,45 @@
+package com.allybros.superego.activity;
+
+import android.content.ClipData;
+
+import android.content.ClipboardManager;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.allybros.superego.R;
+import com.allybros.superego.api.LoadProfileTask;
+import com.allybros.superego.api.LoginTask;
+import com.allybros.superego.util.Trait;
+import com.allybros.superego.util.User;
+import com.beardedhen.androidbootstrap.BootstrapProgressBar;
+
+import java.util.ArrayList;
+
+public class ResultsFragment extends Fragment {
+
+    public ResultsFragment() {
+// Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_results, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
