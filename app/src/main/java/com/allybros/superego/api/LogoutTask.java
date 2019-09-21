1209878cbcfd17e680e19359ebcffcf3acd06a85
orcun <orcunkamiloglu@gmail.com>
2019-09-21 13:45:25 +0300

    Some error fixed and added result screen

---
+++ app/src/main/java/com/allybros/superego/api/LogoutTask.java
@@ -0,0 +1,82 @@
+package com.allybros.superego.api;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import com.allybros.superego.R;
+import com.allybros.superego.activity.LoginActivity;
+import com.allybros.superego.activity.SplashActivity;
+import com.allybros.superego.unit.ErrorCodes;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LogoutTask {
+
+    public static void logoutTask(final Context currentContext, final String session_token) {
+
+        final String LOGIN_URL ="https://api.allybros.com/superego/logout.php";
+        final String logoutSuccesful= (String) currentContext.getString(R.string.logoutSuccesful);
+        final String sysFail=(String)currentContext.getString(R.string.connection_error);
+
+        final Intent mainActivityIntent=new Intent(currentContext, LoginActivity.class);
+        mainActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        RequestQueue queue = Volley.newRequestQueue(currentContext);
+
+        StringRequest jsonRequest=new StringRequest(Request.Method.POST, LOGIN_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                int status;
+
+                try {
+                    JSONObject jsonObj=new JSONObject(response);
+                    status = jsonObj.getInt("status");
+                    switch (status){
+                        case ErrorCodes.SYSFAIL:
+                            Toast.makeText(currentContext, sysFail, Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case ErrorCodes.SUCCESS:
+
+                            Toast.makeText(currentContext, logoutSuccesful,Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(currentContext,sysFail, Toast.LENGTH_SHORT);
+
+            }
+        }) {
+            @Override
+            protected Map<String,String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("session-token", session_token);
+                return params;
+            }
+        };
+
+
+        queue.add(jsonRequest);
+
+    }
+
+
+}
--- app/src/main/java/com/allybros/superego/unit/Trait.java
+++ app/src/main/java/com/allybros/superego/unit/Trait.java
@@ -3,9 +3,68 @@ package com.allybros.superego.unit;
 public class Trait {
     private int traitNo;
     private float value;
+    private String positiveName;
+    private String negativeName;
+    private String positiveIconURL;
+    private String negativeIconURL;
+
+    public Trait(int traitNo, String positiveName, String negativeName, String positiveIconURL, String negativeIconURL) {
+        this.traitNo = traitNo;
+        this.positiveName = positiveName;
+        this.negativeName = negativeName;
+        this.positiveIconURL = positiveIconURL;
+        this.negativeIconURL = negativeIconURL;
+    }
 
     public Trait(int traitNo, float value) {
         this.traitNo = traitNo;
         this.value = value;
+    }
+    public int getTraitNo() {
+        return traitNo;
+    }
+
+    public void setTraitNo(int traitNo) {
+        this.traitNo = traitNo;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public String getPositiveName() {
+        return positiveName;
+    }
+
+    public void setPositiveName(String positiveName) {
+        this.positiveName = positiveName;
+    }
+
+    public String getNegativeName() {
+        return negativeName;
+    }
+
+    public void setNegativeName(String negativeName) {
+        this.negativeName = negativeName;
+    }
+
+    public String getPositiveIconURL() {
+        return positiveIconURL;
+    }
+
+    public void setPositiveIconURL(String positiveIconURL) {
+        this.positiveIconURL = positiveIconURL;
+    }
+
+    public String getNegativeIconURL() {
+        return negativeIconURL;
+    }
+
+    public void setNegativeIconURL(String negativeIconURL) {
+        this.negativeIconURL = negativeIconURL;
     }
 }
