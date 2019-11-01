fd0c8828b643796f1e8023f6d6becb485ab89874
orcun <orcunkamiloglu@gmail.com>
2019-11-01 21:33:42 +0300

    Login screen material desing changed

--- app/src/main/java/com/allybros/superego/activity/LoginActivity.java
+++ app/src/main/java/com/allybros/superego/activity/LoginActivity.java
@@ -1,40 +1,51 @@
 package com.allybros.superego.activity;
 
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.sip.SipSession;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.allybros.superego.R;
 import com.allybros.superego.api.LoadProfileTask;
 import com.allybros.superego.api.LoginTask;
+import com.allybros.superego.unit.ErrorCodes;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btLogin;
     private Button btRegister;
     private EditText etMail;
     private EditText etPassword;
+    public TextInputLayout passwordTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
         btLogin=(Button) findViewById(R.id.btLogin);
         btRegister=(Button) findViewById(R.id.btRegister);
         etMail=(EditText)findViewById(R.id.etMail);
         etPassword=(EditText)findViewById(R.id.etPassword);
-
+        passwordTextInput=(TextInputLayout)findViewById(R.id.password_text_input);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter("status-share"));
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LoginTask.loginTask(getApplicationContext(),etMail.getText().toString(),etPassword.getText().toString());
+
 
             }
         });
@@ -48,5 +59,39 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
     }
+    // Our handler for received Intents. This will be called whenever an Intent
+// with an action named "custom-event-name" is broadcasted.
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int status = intent.getIntExtra("status",0);
+            Log.d("receiver", "Got message: " + status);
+
+            switch (status){
+                case ErrorCodes.SYSFAIL:
+                    passwordTextInput.setError(getString(R.string.loginFailed));
+
+                    break;
+                case ErrorCodes.USERNAME_EMPTY:
+                    passwordTextInput.setError(getString(R.string.usernameEmpty));
+
+                    break;
+
+                case ErrorCodes.PASSWORD_EMPTY:
+                    passwordTextInput.setError(getString(R.string.passwordEmpty));
+
+                    break;
+            }
+
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+        super.onDestroy();
+    }
+
 }
