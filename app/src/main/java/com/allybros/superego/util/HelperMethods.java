7178f7578a352ec1bc6f343e39af66bf08145894
orcun <orcunkamiloglu@gmail.com>
2020-05-18 15:57:57 +0300

    Image refresh problem solved

--- app/src/main/java/com/allybros/superego/util/HelperMethods.java
+++ app/src/main/java/com/allybros/superego/util/HelperMethods.java
@@ -2,13 +2,19 @@ package com.allybros.superego.util;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.util.Base64;
 import android.util.Log;
+
+import androidx.annotation.RequiresApi;
 
 import com.allybros.superego.R;
 import com.android.volley.toolbox.ImageLoader;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Date;
 
 public class HelperMethods {
 
@@ -19,11 +25,22 @@ public class HelperMethods {
         return Base64.encodeToString(imgByte,Base64.DEFAULT);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public static void imageLoadFromUrl(Context context, String URL, CircledNetworkImageView imageView){
+        URL = URL+"&"+HelperMethods.getDate();
         Log.d("imageLoadFromUrl","Image Loading from: "+URL);
         ImageLoader mImageLoader;
         mImageLoader = CustomVolleyRequestQueue.getInstance(context).getImageLoader();
         mImageLoader.get(URL, ImageLoader.getImageListener(imageView, R.drawable.simple_profile_photo, android.R.drawable.ic_dialog_alert));
         imageView.setImageUrl(URL, mImageLoader);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static String getDate(){
+        Date c = Calendar.getInstance().getTime();
+        System.out.println("Current time => " + c);
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(c);
+        System.out.println("Formatted => " + formattedDate);
+        return formattedDate;
     }
 }
