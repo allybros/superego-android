36d40b09b89d570bbb164d14426b2d066618fbb6
orcun <orcunkamiloglu@gmail.com>
2020-05-25 07:15:21 +0300

    Some correction

--- app/src/main/java/com/allybros/superego/activity/EditProfileActivity.java
+++ app/src/main/java/com/allybros/superego/activity/EditProfileActivity.java
@@ -5,21 +5,17 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.drm.DrmStore;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -34,7 +30,6 @@ import com.allybros.superego.util.HelperMethods;
 import com.allybros.superego.util.RequestForGetImage;
 import com.allybros.superego.util.SessionManager;
 import com.android.volley.toolbox.ImageLoader;
-import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
@@ -52,44 +47,41 @@ public class EditProfileActivity extends AppCompatActivity {
     //TODO: Why not private?
     private MaterialProgressBar progressEditProfile;
     private ConstraintLayout cardFormEditProfile;
-    TextInputEditText username,email,information;
-    TextInputLayout etUsername_text_input,etEmail_text_input,etInformation_text_input;
+    private TextInputEditText username,email,information;
+    private TextInputLayout etUsername_text_input,etEmail_text_input,etInformation_text_input;
     private SlidrInterface slidr;
-    Button btChangePhoto;
-    CircledNetworkImageView settingsImage;
-    ConstraintLayout editProfileLayout;
+    private Button btChangePhoto;
+    private CircledNetworkImageView settingsImage;
+    private ConstraintLayout editProfileLayout;
     public static Uri newImagePath=null;
 
     private Button btnSaveProfile;
 
-    private final int IMG_REQUEST=1; //TODO: Add description. This is not a usual thing.
-    //TODO: Add free space before each method
+    private final int IMG_REQUEST=1;    //Needs for image selection from local storage
+
+
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        //TODO: Redundant casting. Place spaces when using '=' Ex: a = 5 not a= 5
-        //TODO: Building view hierarchy here is a valid operation. You can keep them here.
         editProfileLayout = findViewById(R.id.editProfileLayout);
         progressEditProfile = findViewById(R.id.progressEditProfile);
-        cardFormEditProfile =findViewById(R.id.cardFormEditProfile);
-        btChangePhoto= findViewById(R.id.btChangePhoto);
-        username= findViewById(R.id.etUsername);
-        email= findViewById(R.id.etEmail);
-        information= findViewById(R.id.etInformation);
-        etUsername_text_input= findViewById(R.id.textInputUsername);
-        etEmail_text_input= findViewById(R.id.textInputEmail);
-        etInformation_text_input= findViewById(R.id.etInformation_text_input);
+        cardFormEditProfile = findViewById(R.id.cardFormEditProfile);
+        btChangePhoto = findViewById(R.id.btChangePhoto);
+        username = findViewById(R.id.etUsername);
+        email = findViewById(R.id.etEmail);
+        information = findViewById(R.id.etInformation);
+        etUsername_text_input = findViewById(R.id.textInputUsername);
+        etEmail_text_input = findViewById(R.id.textInputEmail);
+        etInformation_text_input = findViewById(R.id.etInformation_text_input);
         settingsImage = findViewById(R.id.imageSettings);
         btnSaveProfile = findViewById(R.id.btnSaveProfile);
 
         LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(updateInformationReceiver, new IntentFilter(ConstantValues.ACTION_UPDATE_INFORMATION));
         LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(updateImageReceiver, new IntentFilter(ConstantValues.ACTION_UPDATE_IMAGE));
 
-        //TODO: Initializing Buttons, TextFields, AvatarChooser tasks are separated and there are no shared dependency.
-        // There is no need to keep them together. Separate them to subroutines, in java methods, for making easier to modify.
         btChangePhoto.bringToFront();
         btChangePhoto.invalidate();
 
@@ -108,8 +100,7 @@ public class EditProfileActivity extends AppCompatActivity {
         String URL= ConstantValues.AVATAR_URL+SessionManager.getInstance().getUser().getImage();
         ImageLoader mImageLoader;
         mImageLoader = RequestForGetImage.getInstance(getApplicationContext()).getImageLoader();
-        //TODO: Use default avatar
-        mImageLoader.get(URL, ImageLoader.getImageListener(settingsImage, R.drawable.simple_profile_photo, android.R.drawable.ic_dialog_alert));
+        mImageLoader.get(URL, ImageLoader.getImageListener(settingsImage, R.drawable.img_avatar, android.R.drawable.ic_dialog_alert));
         settingsImage.setImageUrl(URL, mImageLoader);
 
         slidr= Slidr.attach(this);
@@ -289,7 +280,6 @@ public class EditProfileActivity extends AppCompatActivity {
         }
     };
 
-    //TODO: Clear redundant blank spaces in order to make reading easier. Just add 1 blank space after break statement.
     private BroadcastReceiver updateImageReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -300,20 +290,16 @@ public class EditProfileActivity extends AppCompatActivity {
             //Check status
             switch (status){
                 case ErrorCodes.SUCCESS:
-
                     new MaterialAlertDialogBuilder(EditProfileActivity.this)
                             .setTitle("insightof.me")
                             .setMessage(getApplicationContext().getString(R.string.processComplated))
                             .setPositiveButton(getString(R.string.action_ok), new DialogInterface.OnClickListener() {
                                 @Override
-                                public void onClick(DialogInterface dialog, int which) {
-
-                                }
+                                public void onClick(DialogInterface dialog, int which) {}
                             }).show();
                     break;
 
                 case ErrorCodes.SYSFAIL:
-
                     new MaterialAlertDialogBuilder(EditProfileActivity.this)
                             .setTitle("insightof.me")
                             .setMessage(getApplicationContext().getString(R.string.connection_error))
@@ -322,7 +308,6 @@ public class EditProfileActivity extends AppCompatActivity {
                     break;
 
                 case ErrorCodes.INVALID_FILE_EXTENSION:
-
                     new MaterialAlertDialogBuilder(EditProfileActivity.this)
                             .setTitle("insightof.me")
                             .setMessage(getApplicationContext().getString(R.string.invalid_file_extension)+" "+getApplicationContext().getString(R.string.connection_error)+status)
@@ -331,7 +316,6 @@ public class EditProfileActivity extends AppCompatActivity {
                     break;
 
                 case ErrorCodes.INVALID_FILE_TYPE:
-
                     new MaterialAlertDialogBuilder(EditProfileActivity.this)
                             .setTitle("insightof.me")
                             .setMessage(getApplicationContext().getString(R.string.invalid_file_type)+" "+getApplicationContext().getString(R.string.connection_error)+status)
@@ -340,7 +324,6 @@ public class EditProfileActivity extends AppCompatActivity {
                     break;
 
                 case ErrorCodes.INVALID_FILE_SIZE:
-
                     new MaterialAlertDialogBuilder(EditProfileActivity.this)
                             .setTitle("insightof.me")
                             .setMessage(getApplicationContext().getString(R.string.invalid_file_size)+" "+getApplicationContext().getString(R.string.connection_error)+status)
@@ -349,14 +332,12 @@ public class EditProfileActivity extends AppCompatActivity {
                     break;
 
                 case ErrorCodes.FILE_WRITE_ERROR:
-
                     new MaterialAlertDialogBuilder(EditProfileActivity.this)
                             .setTitle("insightof.me")
                             .setMessage(getApplicationContext().getString(R.string.connection_error)+" "+getApplicationContext().getString(R.string.connection_error)+status)
                             .setPositiveButton(getString(R.string.action_ok), null)
                             .show();
                     break;
-
             }
         }
     };
@@ -365,7 +346,6 @@ public class EditProfileActivity extends AppCompatActivity {
     @Override
     public void onDestroy() {
         LocalBroadcastManager.getInstance(EditProfileActivity.this).unregisterReceiver(updateImageReceiver);
-
         LocalBroadcastManager.getInstance(EditProfileActivity.this).unregisterReceiver(updateInformationReceiver);
         Log.d("SettingsDestroy","RUN");
         super.onDestroy();
@@ -381,6 +361,7 @@ public class EditProfileActivity extends AppCompatActivity {
         }else{
             HelperMethods.imageLoadFromUrl(getApplicationContext(), ConstantValues.AVATAR_URL+SessionManager.getInstance().getUser().getImage(),settingsImage);
         }
+
         Log.d("SettingsResume","RUN");
 
     }
