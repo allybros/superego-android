30a2fdcf7e4882938b32c80d41676bf6aa8a728a
umutalacam <umutcanalacam@gmail.com>
2020-07-14 01:50:37 +0300

    Set orientation

--- app/src/main/java/com/allybros/superego/activity/LoginActivity.java
+++ app/src/main/java/com/allybros/superego/activity/LoginActivity.java
@@ -66,7 +66,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         initializeComponents();
         setupReceivers();
         setupUi();
