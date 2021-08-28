1034d17c68ddbc1499e93b281ef661db95d026a9
Orcun Kamiloglu <orcunkamiloglu@allybros.com>
2021-08-28 16:26:12 +0300

    Direction added to conditions ad key edited

--- app/src/main/java/com/allybros/superego/activity/RegisterActivity.java
+++ app/src/main/java/com/allybros/superego/activity/RegisterActivity.java
@@ -4,7 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -176,7 +178,16 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
-
+        
+        tvAgreementRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = getResources().getString(R.string.conditions_link);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 
     /**
