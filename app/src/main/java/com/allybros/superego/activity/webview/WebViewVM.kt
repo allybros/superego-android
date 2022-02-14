f57ee6681a987f3799a69568ba45cdd994674f2f
Orcun Kamiloglu <orcunkamiloglu@gmail.com>
2022-02-14 14:14:44 +0300

    Webview - data binding and viewmodel were added

---
+++ app/src/main/java/com/allybros/superego/activity/webview/WebViewVM.kt
@@ -0,0 +1,7 @@
+package com.allybros.superego.activity.webview
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class WebViewVM(application: Application) : AndroidViewModel(application) {
+}
