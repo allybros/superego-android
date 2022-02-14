c58654ec0fb958a1b35dc5c12863feb2dbc67714
Orcun Kamiloglu <orcunkamiloglu@gmail.com>
2022-02-14 13:49:46 +0300

    Register - data binding and viewmodel were added

---
+++ app/src/main/java/com/allybros/superego/activity/register/RegisterVM.kt
@@ -0,0 +1,7 @@
+package com.allybros.superego.activity.register
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class RegisterVM(application: Application) : AndroidViewModel(application) {
+}
