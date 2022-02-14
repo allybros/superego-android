bdaf673185423bc2efbab929da2c6dd4a6283d21
Orcun Kamiloglu <orcunkamiloglu@gmail.com>
2022-02-14 14:09:05 +0300

    Userpage - data binding and viewmodel were added

---
+++ app/src/main/java/com/allybros/superego/activity/userpage/UserPageVM.kt
@@ -0,0 +1,7 @@
+package com.allybros.superego.activity.userpage
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class UserPageVM(application: Application) : AndroidViewModel(application) {
+}
